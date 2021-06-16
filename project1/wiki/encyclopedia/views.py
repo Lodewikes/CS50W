cd5c67446b6887c304915566b6b2a6714e35bd4e
Louw <greyvenstein.louw@gmail.com>
2021-06-16 12:01:19 +0200

    get_entry implementation

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
 
@@ -8,3 +8,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+# display a wiki
+def get_wiki(request, title):
+    markdowner = Markdown()
+    entry = util.get_entry(title=title)
+    if entry is None:
+        # TODO render an error page showing the title is unavailable
+        return render(request, "encyclopedia/entryNotExists.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/wikiPage.html", {
+            "entry": markdowner.convert(entry),
+            "title": title
+        })

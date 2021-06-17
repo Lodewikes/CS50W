873a39a5cb3f570d1f0832be396bb513d4c549e9
Louw <greyvenstein.louw@gmail.com>
2021-06-17 16:17:11 +0200

    all url paths defined

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.get_wiki, name="get_wiki"),
+    path("wiki/<str:title>/edit", views.edit_entry, name="edit"),
+    path("create", views.new_entry, name="new_entry"),
+    path("search", views.search, name="search"),
+    path("random", views.random_entry, name="random"),
 ]

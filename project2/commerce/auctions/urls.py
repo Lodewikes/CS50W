38a4f62e2415929d4ea76556a7c1e1eb91ec8d62
Louw <greyvenstein.louw@gmail.com>
2021-06-22 12:48:10 +0200

    create paths for listings/closed, listings/create and categories

--- project2/commerce/auctions/urls.py
+++ project2/commerce/auctions/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("listings/closed", views.closed_listings, name="closed_listings"),
+    path("listings/create", views.create_listing, name="create_listing"),
+    path("categories", views.categories, name="categories")
 ]

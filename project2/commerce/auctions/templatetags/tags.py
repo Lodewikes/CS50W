97e4a987d3218fc6b896f56422b48a9b647dd20f
Louw <greyvenstein.louw@gmail.com>
2021-06-23 17:33:29 +0200

    implements simple_tags for getting watchlist status (boolean) and getting listing item from watchlist primary key

--- project2/commerce/auctions/templatetags/tags.py
+++ project2/commerce/auctions/templatetags/tags.py
@@ -4,10 +4,16 @@ from auctions.models import *
 register = template.Library()
 
 
-@register.assignment_tag(takes_context=True)
+@register.simple_tag
 def get_watchlist_status(user, pk):
     obj = WatchList.objects.filter(user=user, listing_pk=int(pk))
     if obj:
         return True
     else:
         return False
+
+
+@register.simple_tag
+def get_watchlist_item(pk):
+    obj = AuctionListing.objects.get(id=int(pk))
+    return obj

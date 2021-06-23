26b60bcdbaee9d6d17287763b12cdf90bc1d6ebd
Louw <greyvenstein.louw@gmail.com>
2021-06-23 17:34:28 +0200

    update

--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -7,12 +7,37 @@ class User(AbstractUser):
 
 
 class AuctionListing(models.Model):
-    pass
+    seller = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    start_bid = models.FloatField()
+    listing_date = models.DateTimeField(auto_now_add=True)
+
+    # Optional fields
+    image = models.CharField(max_length=2048, default=None, blank=True, null=True)
+    category = models.CharField(max_length=64, default="uncategorized", blank=True)
+
+    def __str__(self):
+        return f"Listing: {self.title} by {self.seller}"
 
 
-class Bids(models.Model):
-    pass
+class Bid(models.Model):
+    listing_pk = models.IntegerField()
+    user = models.CharField(max_length=64)
+    bid = models.FloatField()
 
 
-class Comments(models.Model):
+class Comment(models.Model):
+    user = models.CharField(max_length=64)
+    comment = models.CharField(max_length=1000)
+    listing_pk = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+
+
+class WatchList(models.Model):
+    user = models.CharField(max_length=64)
+    listing_pk = models.IntegerField()
+
+
+class Sold(models.Model):
     pass

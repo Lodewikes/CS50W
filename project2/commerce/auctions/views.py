693b16f207bbc54c6b78e56bc6669df848de71cf
Louw <greyvenstein.louw@gmail.com>
2021-06-25 08:46:41 +0200

    bid view implemented

--- project2/commerce/auctions/views.py
+++ project2/commerce/auctions/views.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timezone
+
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
@@ -12,6 +15,7 @@ from .models import *
 def index(request):
     return render(request, "auctions/index.html", {
         "listings": AuctionListing.objects.all(),
+        "bids": Bid.objects.all()
     })
 
 
@@ -99,13 +103,54 @@ def add_to_watchlist(request, listing_id):
 
     if item:
         item.delete()
-        print("removed")
-        # return render(request, "auctions/index.html", {
-        #     "listings": AuctionListing.objects.all(),
-        # })
         return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
     else:
         new_item = WatchList(user=request.user.username, listing_pk=listing_id)
         new_item.save()
-        print("added")
         return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+
+@login_required(login_url="/login")
+def post_comment(request, listing_id):
+    if request.method == "POST":
+        comment = request.POST.get("text", False)
+        user = request.user.username
+        time = datetime.datetime.now(timezone.utc)
+        comment_obj = Comment(user=user, comment=comment, listing_pk=int(listing_id), time=time)
+        if user is not None:
+            comment_obj.save()
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+        else:
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+
+def bid(request, listing_id):
+    if request.method == "POST":
+        bids = Bid.objects.filter(listing_pk=listing_id)
+        highest_bid = None
+        if bids:
+            for bidding in bids:
+                if highest_bid is None or bidding.bid > highest_bid:
+                    highest_bid = bidding.bid
+        else:
+            highest_bid = 0.00
+
+        bid = Bid(listing_pk=int(listing_id), user=request.user.username, bid=request.POST.get("bid", False))
+        if request.user.username is not None and highest_bid is None and float(bid.bid) > float(AuctionListing.objects.get(id=int(listing_id)).start_bid):
+            bid.save()
+            return render(request, "auctions/listing.html", {
+                "listing": AuctionListing.objects.get(pk=int(listing_id)),
+                "comments": Comment.objects.all().filter(listing_pk=int(listing_id)),
+            })
+        elif request.user.username is not None and float(bid.bid) > highest_bid:
+            bid.save()
+            return render(request, "auctions/listing.html", {
+                "listing": AuctionListing.objects.get(pk=int(listing_id)),
+                "comments": Comment.objects.all().filter(listing_pk=int(listing_id)),
+            })
+        else:
+            return render(request, "auctions/listing.html", {
+                "listing": AuctionListing.objects.get(pk=int(listing_id)),
+                "comments": Comment.objects.all().filter(listing_pk=int(listing_id)),
+                "failed_bid_message": "Bid must be higher than the starting and current bid"
+            })

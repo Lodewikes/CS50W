76a60a0504757f76b2b43777e402c048ffc75f9c
Louw <greyvenstein.louw@gmail.com>
2021-06-25 08:45:58 +0200

    path to bid

--- project2/commerce/auctions/urls.py
+++ project2/commerce/auctions/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path("listings/<str:listing_id>", views.listing_view, name="listing"),
     path("categories", views.categories, name="categories"),
     path("watchlist/add/<str:listing_id>", views.add_to_watchlist, name="add_to_watchlist"),
-    path("watchlist", views.watchlist_view, name="watchlist")
+    path("watchlist", views.watchlist_view, name="watchlist"),
+    path("listing/comment/<str:listing_id>", views.post_comment, name="post_comment"),
+    path("listing/bid/<str:listing_id>", views.bid, name="bid")
 ]

5ab0aa11dda27f5fba04d1213e5e55414df9fffe
Louw <greyvenstein.louw@gmail.com>
2021-07-13 10:00:57 +0200

    no change

--- project3/mail/mail/models.py
+++ project3/mail/mail/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
 
@@ -27,3 +26,4 @@ class Email(models.Model):
             "read": self.read,
             "archived": self.archived
         }
+

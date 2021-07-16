5d075bb4dc2d4b833be98239061483f72b294634
Louw <greyvenstein.louw@gmail.com>
2021-07-16 16:36:17 +0200

    implement Post model

--- project4/project4/network/models.py
+++ project4/project4/network/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    poster = models.CharField(max_length=64)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField()
+    body = models.TextField()
+
+    def __str__(self):
+        return f"Post by {self.poster}"
+
+
+# TODO determine necessity at later stage
+# class Follower(models.Model):
+#     pass

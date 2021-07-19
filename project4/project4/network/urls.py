dc8a91c529d4a17082c63a3e792e0bf28f058275
Louw <greyvenstein.louw@gmail.com>
2021-07-19 09:58:16 +0200

    profile page path

--- project4/project4/network/urls.py
+++ project4/project4/network/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("posts/post", views.submit_post, name="post")
+    path("posts/post", views.submit_post, name="post"),
+    path("profile/<str:profile_name>", views.profile_view, name="profiles"),
 ]

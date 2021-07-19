418eca511a312678a8cd746f940dd009f5217626
Louw <greyvenstein.louw@gmail.com>
2021-07-19 15:35:26 +0200

    implement loadPosts and displayPosts functions

--- project4/project4/network/static/network/index.js
+++ project4/project4/network/static/network/index.js
@@ -1,3 +1,55 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // TODO
+    loadPosts();
 });
+
+function loadPosts() {
+    document.querySelector("#list-posts").style.display = "block";
+
+    fetch(`/posts`)
+    .then(response => response.json())
+    .then(posts => {
+        posts.forEach(post => {
+            const item = document.createElement("div");
+            displayPosts(post, item);
+            document.querySelector("#list-posts").appendChild(item);
+        })
+    })
+}
+
+function displayPosts(post, item) {
+    poster = document.createElement("strong");
+    timestamp = document.createElement("p");
+    likes = document.createElement("p");
+    body = document.createElement("p");
+    likeBtn = document.createElement("button");
+
+    // username
+    poster.innerHTML = post.poster;
+    poster.setAttribute("class", "like-btn")
+    poster.addEventListener("click", () => displayProfile(post))
+    
+    // timestamp
+    timestamp.innerHTML = post.timestamp;
+
+    // body
+    body.innerHTML = post.body;
+
+    // likes
+    likes.innerHTML = post.likes;
+
+    // like button
+    likeBtn.textContent = "Like";
+    likeBtn.setAttribute("class", "btn btn-dark");
+
+    item.appendChild(poster);
+    item.appendChild(timestamp);
+    item.appendChild(likes);
+    item.appendChild(body);
+    item.appendChild(likeBtn);
+    item.appendChild(document.createElement("hr"));
+}
+
+function displayProfile(post) {
+    document.querySelector("#list-posts").innerHTML = "";
+    document.querySelector("#list-posts").innerHTML = post.poster;
+}

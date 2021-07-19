eced88904eb6e727a797d005f9185bb4e91261cd
Louw <greyvenstein.louw@gmail.com>
2021-07-19 15:32:35 +0200

    update urls

--- project4/project4/network/urls.py
+++ project4/project4/network/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("posts/post", views.submit_post, name="post"),
+    path("posts", views.get_all_posts, name="get_posts"),
     path("profile/<str:profile_name>", views.profile_view, name="profiles"),
 ]

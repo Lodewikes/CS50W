461d6329cbc0dbd9c3717b5a5f449df961324a63
Louw <greyvenstein.louw@gmail.com>
2021-07-21 17:20:27 +0200

    url for likes api

--- project4/project4/network/urls.py
+++ project4/project4/network/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path("posts", views.get_all_posts, name="get_posts"),
     path("posts/<str:post_id>", views.get_post_by_id, name="get_post_bt_id"),
     path("profile/<str:profile_name>", views.profile_view, name="profiles"),
+    path("posts/likes/<str:post_id>", views.get_all_likes_by_post_id, name="post_likes"),
+    path("posts/likes/<str:post_id>/<str:user_id>", views.get_like_by_post_and_user_id, name="post_like_by_user"),
 ]

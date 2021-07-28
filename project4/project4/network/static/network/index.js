a60472c083d68bdaa32b05bc891cf036e943f424
Louw <greyvenstein.louw@gmail.com>
2021-07-28 18:26:54 +0200

    set likes function finished; edit post function working but requires additional feature to revert back to paragraph

--- project4/project4/network/static/network/index.js
+++ project4/project4/network/static/network/index.js
@@ -10,18 +10,25 @@ function loadPosts() {
     .then(posts => {
         posts.forEach(post => {
             const item = document.createElement("div");
-            displayPosts(post, item);
+            item.setAttribute("id", "post-" + post.id);
+            setLikes(post);
+            displayPost(post, item);
             document.querySelector("#list-posts").appendChild(item);
         })
     })
+    .catch(error => console.log(error));
 }
 
-function displayPosts(post, item) {
+function displayPost(post, item) {
+    const user_id = JSON.parse(document.getElementById("user_id").textContent);
+    const user_name = JSON.parse(document.getElementById("user_name").textContent);
     poster = document.createElement("strong");
     timestamp = document.createElement("p");
     likes = document.createElement("p");
+    bodyDiv = document.createElement("div");
     body = document.createElement("p");
     likeBtn = document.createElement("button");
+    editPostBtn = document.createElement("button");
 
     // username
     poster.innerHTML = post.poster;
@@ -33,26 +40,56 @@ function displayPosts(post, item) {
 
     // body
     body.innerHTML = post.body;
+    body.setAttribute("id", "post-body-" + post.id);
     body.style.whiteSpace = "pre-wrap";
+    bodyDiv.setAttribute("id", "body-div-" + post.id)
+    bodyDiv.appendChild(body);
 
     // likes
-    likes.innerHTML = post.likes;
     likes.setAttribute("id", "post-likes-" + post.id)
+    likes.innerHTML = post.likes;
 
     // like button
-    if (likeBtn.textContent !== "Unlike") {
-        likeBtn.textContent = "Like";
-    }
+
     likeBtn.setAttribute("class", "btn btn-dark");
     likeBtn.setAttribute("id", "likeBtn-" + post.id)
-    likeBtn.addEventListener("click", () => likePost(post, likes))
+    likeBtn.style.marginRight = "5px";
+    // set initial button text context
+    fetch(`posts/likes/${post.id}/${user_id}`)
+    .then(response => response.json())
+    .then(like => {
+        if(!like.error){
+            if (!like.unliked) {
+                document.getElementById("likeBtn-" + post.id).textContent = "Unlike";
+            }
+            else {
+                document.getElementById("likeBtn-" + post.id).textContent = "Like";
+            }
+        }
+        else {
+            document.getElementById("likeBtn-" + post.id).textContent = "Like";
+        }
+    })
+    likeBtn.addEventListener("click", () => likePost(post));
 
+    // editPostBtn
+    if (post.poster === user_name){
+        editPostBtn.setAttribute("class", "btn btn-dark");
+        editPostBtn.setAttribute("id", "editPostBtn-" + post.id);
+        editPostBtn.textContent = "Edit";
+        editPostBtn.addEventListener("click", () => editPost(post));
+    }
+    
+
+    item.appendChild(document.createElement("hr"));
     item.appendChild(poster);
     item.appendChild(timestamp);
     item.appendChild(likes);
-    item.appendChild(body);
+    item.appendChild(bodyDiv);
     item.appendChild(likeBtn);
-    item.appendChild(document.createElement("hr"));
+    if (post.poster === user_name) {
+        item.appendChild(editPostBtn);
+    }
 }
 
 function displayProfile(post) {
@@ -60,29 +97,34 @@ function displayProfile(post) {
     document.querySelector("#list-posts").innerHTML = post.poster;
 }
 
-async function likePost(post, likesElement) {
+// likeBtn onclick handler
+async function likePost(post) {
     const user_id = JSON.parse(document.getElementById("user_id").textContent);
-
-    // does like exist?
-    // if so, unlike (PUT)
-    // if not like the post (POST)
-    // add or subract from total likes
 
     await fetch(`posts/likes/${post.id}/${user_id}`)
     .then(response => response.json())
     .then(like => {
         if (like.user_pk === undefined && like.post_pk === undefined && like.unliked === undefined) {
             likeFetch(post.id, user_id, "POST", false);
+            console.log("create like");
+            document.getElementById("likeBtn-" + post.id).textContent = "Unlike";
         }
         else if(like.user_pk !== undefined && like.post_pk !== undefined && like.unliked === false) {
             likeFetch(post.id, user_id, "PUT", true);
+            console.log("unlike");
+            document.getElementById("likeBtn-" + post.id).textContent = "Like";
         }
         else if(like.user_pk !== undefined && like.post_pk !== undefined && like.unliked === true) {
             likeFetch(post.id, user_id, "PUT", false);
+            console.log("like");
+            document.getElementById("likeBtn-" + post.id).textContent = "Unlike";
         } 
-    })  
+        console.log("setting likes")
+        setLikes(post);
+    });
 }
 
+// Called in likePost()
 async function likeFetch(post_id, user_id, method, likeBool) {
     await fetch(`posts/likes/${post_id}/${user_id}`, {
         method: method,
@@ -90,8 +132,81 @@ async function likeFetch(post_id, user_id, method, likeBool) {
             user_pk: user_id,
             post_pk: post_id,
             unliked: likeBool
-        }),
+        })
     })
+    .then(response => response.json()) 
+    .catch(error => console.log(error)); // FIXME SyntaxError: JSON.parse: unexpected end of data at line 1 column 1 of the JSON data index.js:132:29
+}
+
+async function setLikes(post) {
+    var count = 0;
+    await fetch(`posts/likes/${post.id}`)
     .then(response => response.json())
+    .then(likes => {
+        likes.forEach(like => {
+            if (like.unliked === false) {
+                count++;
+            }
+        });
+        fetch(`posts/${post.id}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                likes: count
+            })
+        });
+        document.getElementById("post-likes-" + post.id).innerHTML = post.likes;
+    })
+    .catch(error => console.log(error));
+}
+
+function editPost(post) {
+    // get the elements to change
+    const item = document.getElementById("post-" + post.id);
+    const bodyDiv = document.getElementById("body-div-" + post.id);
+    const body = document.getElementById("post-body-" + post.id);
+    const editBtn = document.getElementById("editPostBtn-" + post.id);
+    const saveBtn = document.createElement("button");
+
+    // store text from body <p>
+    var text = body.textContent;
+
+    // remove the body <p>
+    bodyDiv.removeChild(body);
+
+    // replace editBtn with saveBtn
+    item.removeChild(editBtn);
+    saveBtn.setAttribute("class", "btn btn-primary");
+    saveBtn.setAttribute("id", "save-edit-btn-" + post.id);
+    saveBtn.textContent = "Save";
+    item.appendChild(saveBtn);
+
+
+    // create <textarea> to replace <p>
+    const textarea = document.createElement("textarea");
+    textarea.setAttribute("class", "form-control");
+    textarea.setAttribute("id", "edit-post-textarea-" + post.id)
+    textarea.textContent = text;
+    textarea.style.marginBottom = "8px";
+    textarea.style.width = "50%";
+    textarea.style.whiteSpace = "pre-wrap"
+
+    saveBtn.addEventListener("click", () => saveEditedPost(post))
+    
+    bodyDiv.appendChild(textarea);
+}
+
+async function saveEditedPost(post) {
+    var textcontent = document.getElementById("edit-post-textarea-" + post.id).value;
+    alert(textcontent)
+    await fetch(`posts/${post.id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+            poster: post.poster,
+            timestamp: post.timestamp,
+            likes: post.likes,
+            body: textcontent
+        })
+    })
+    .then(response => response.json()) 
     .catch(error => console.log(error));
 }
